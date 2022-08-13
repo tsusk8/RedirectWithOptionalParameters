34e9b6de4b2e707a17d24fe6e87357da1b2fa062
Tsubasa <realmadrid411tsubasa@gmail.com>
2022-08-13 19:04:41 +0900

    add all file

---
+++ setFile.js
@@ -0,0 +1,20 @@
+// init
+/**
+ * url = {
+ *  [
+ *      target=url => [
+ *          addParam1,
+ *          addParam2
+ *      ]
+ *  ]
+ * }
+ */
+
+
+// add
+
+// delete
+
+// all delete
+
+localStorage.setItem('current_condtion', 'success');
