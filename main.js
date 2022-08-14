a1f4e8669d54ac3a11a1f9764e0c61e3d5f8d2f8
Tsubasa <realmadrid411tsubasa@gmail.com>
2022-08-14 19:59:36 +0900

    remove unnecessary files

--- main.js
+++ main.js
@@ -1,29 +1,9 @@
-// 現在のURLからURLオブジェクトを生成（例として、https://example.com上で以下Javascriptを実行しているとする）
-const url = new URL(window.location.href);
-
+const url    = new URL(window.location.href);
 const params = new URLSearchParams(location.search);
+/**
+ * Without specific parameters, redirect to URL with parameters
+ */
 if (params.has('ov') === false) {
-    // キーを指定し、クエリパラメータを付与
     url.searchParams.set('ov', '1');
-    console.log(url.href); // https://example.com?addParam=test
-
-    // 遷移する
     location.href = url;
 }
-
-
-// 設定したい値はmatchesするwebサイトとクエリパラメータ
-
-// {
-//     "name": "Sample",
-//     "version": "1.0.0",
-//     "manifest_version": 3,
-//     "description": "Sample Chrome Extension",
-//     "content_scripts": [{
-    // matchesに自動で値を入れる
-//         "matches": ["https://www.google.com/*"],
-//         "js": [
-//             "content.js"
-//         ]
-//     }]
-// }
